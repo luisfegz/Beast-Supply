754e9033290ff1d712816188e84b5af18cf1bbda
Luis Felipe <luisfelipegaitanzambrano@gmail.com>
2024-08-03 19:36:25 -0500

    Upload butto of comprar

--- components/global/NewProducts/ProductBox.js
+++ components/global/NewProducts/ProductBox.js
@@ -1,3 +1,4 @@
+import { Link } from "lucide-react";
 import CartIcon from "../../icons/CartIcon";
 import { BackgroundGradient } from "../../ui/background-gradient";
 import Center from "../Center";
@@ -16,13 +17,13 @@ export default function ProductBox({_id, title, description, price, images}) {
           </p>
           <p className="text-sm text-center mb-3 sm:text-xl mt-2 text-neutral-200">
             {formattedPrice} COP
-          </p>
-          
-          <button className="w-full relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+          </p>  
+          <button className="w-full inline-flex h-12 text-base md:text-xl animate-shimmer items-center justify-center z-40
+                rounded-md border border-zinc-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] 
+                bg-[length:200%_100%] px-3.5 font-normal md:font-medium text-slate-100 transition-colors focus:outline-none 
+                focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+          >
               Comprar <CartIcon className="ml-2 h-3.5 sm:h-3.9 md:h-4.4 lg:h-5" />
-            </span>
           </button>
           {/* Remeber to add the function add to cart 
           <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
