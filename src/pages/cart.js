bd09b874bf1ec0e61c613af368d1173736fbe8dd
Luis Felipe <luisfelipegaitanzambrano@gmail.com>
2024-08-11 11:31:31 -0500

    cart fix Order Information Add text description

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -8,16 +8,17 @@ import { CartContext } from '../../components/global/CartContext';
 
 import Table from '../../components/global//Table.js';
 import axios from 'axios';
+import CartIcon from '../../components/icons/CartIcon';
 
 
 export default function CartPage() {
   const { cartProducts } = useContext(CartContext);
   
-  function formatPrice(value) {
+  {/*function formatPrice(value) {
     return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
   }
 
-  {/*async function goToPayment() {
+  async function goToPayment() {
     const response = await axios.post("/api/checkout", {
       name,
       email,
@@ -94,71 +95,107 @@ export default function CartPage() {
       <main className="flex bg-[#0a0a0a] items-center justify-center flex-col">
         <Navbar />
         <section className="w-full mt-0 inset-0">
-          <div className='text-white mb-10 grid grid-rows-2  md:grid-cols-2 mt-4 md:mt-8 gap-4 md:gap-10 mx-2.5 md:mx-6'>
+          <div className='text-white mb-10 grid grid-rows-2   md:grid-cols-2 mt-4 md:mt-8 gap-[40px] md:gap-10 mx-2.5 md:mx-6'>
+            
             {/* Box Products */}
+
             <div className='bg-neutral-900 rounded-[8.6px] p-5'>
-              <h2>Cart</h2>
               {!cartProducts?.length && (
                 <div>Your Cart is empty</div>
               )}
-            </div>
-            <div className='bg-neutral-900 rounded-[8.6px] p-5'>
-              <h2 className="font-bold text-xl text-neutral-200">
-                Bienvenido a Beast Supply
-              </h2>
-              <p className="text-sm max-w-sm mt-2 text-neutral-300">
-                Déjenos tus datos de contacto para realizar la entrega de tu pedido o de la perona 
-                que recibirá tu pedido.
-              </p>
+              {cartProducts?.length > 0 && (
+                <>
+                  <h2 className="font-bold text-xl flex text-neutral-200">
+                    Carrito de compras
+                    <CartIcon 
+                      className="
+                        mt-[6.6px] ml-2 h-[18px] sm:h-[18px] md:h-4 lg:h-5
+                      "
+                    />
+                  </h2>
 
-              <form className="my-8" >
-                <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-                  <LabelInputContainer>
-                    <Label htmlFor="name">Primer nombre</Label>
-                    <Input id="name" placeholder="Tyler Durden (Ejemplo)" type="text" />
+                  <p className="text-sm max-w-sm mt-2 text-neutral-300">
+                  ¡Mejora tu rendimiento! 💪 Completa tu compra con nuestros suplementos deportivos de alta calidad. ¡No te quedes sin ellos!
+                  </p>
+
+                </>
+              )}
+            </div>
+
+            {/* Box of Order Information */}
+
+            {!!cartProducts?.length && (
+              <div className='bg-neutral-900 rounded-[8.6px] p-5'>
+
+                <h2 className="font-bold text-xl text-neutral-200">
+                  Bienvenido a Beast Supply
+                </h2>
+
+                <p className="text-sm max-w-sm mt-2 text-neutral-300">
+                  Déjenos tus datos de contacto para realizar la entrega de tu pedido o de la perona 
+                  que recibirá tu pedido.
+                </p>
+
+                <form className="my-8" >
+
+                  <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+
+                    <LabelInputContainer>
+                      <Label htmlFor="name">Primer nombre</Label>
+                      <Input id="name" placeholder="Tyler Durden (Ejemplo)" type="text" />
+                    </LabelInputContainer>
+
+                  </div>
+
+                  <LabelInputContainer className="mb-4">
+                    <Label htmlFor="email">Dirección Email</Label>
+                    <Input id="email" placeholder="projectmayhem@fc.com (Ejemplo)" type="email" />
                   </LabelInputContainer>
-                </div>
-                <LabelInputContainer className="mb-4">
-                  <Label htmlFor="email">Dirección Email</Label>
-                  <Input id="email" placeholder="projectmayhem@fc.com (Ejemplo)" type="email" />
-                </LabelInputContainer>
-                <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-                  <LabelInputContainer>
-                    <Label htmlFor="streetAddress">Dirección</Label>
-                    <Input id="streetAddress" placeholder="Cll 9 # 70-90 (Ejemplo)" type="text" />
-                  </LabelInputContainer>
-                  <LabelInputContainer>
-                    <Label htmlFor="city">Ciudad</Label>
-                    <Input id="city" placeholder="Santiago de Cali (Ejemplo)" type="text" />
-                  </LabelInputContainer>
-                </div>
-                 <LabelInputContainer  className='mb-4'>
-                  <LabelInputContainer>
+
+                  <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+
+                    <LabelInputContainer>
+                      <Label htmlFor="streetAddress">Dirección</Label>
+                      <Input id="streetAddress" placeholder="Cll 9 # 70-90 (Ejemplo)" type="text" />
+                    </LabelInputContainer>
+
+                    <LabelInputContainer>
+                      <Label htmlFor="city">Ciudad</Label>
+                      <Input id="city" placeholder="Santiago de Cali (Ejemplo)" type="text" />
+                    </LabelInputContainer>
+
+                  </div>
+
+                  <LabelInputContainer  className='mb-4'>
                       <Label htmlFor="postalCode">Codigo Postal</Label>
                       <Input id="postalCode" placeholder="760032 (Ejemplo)" type="text" />
-                    </LabelInputContainer>
                   </LabelInputContainer>
-                <LabelInputContainer className="mb-4">
-                  <Label htmlFor="streetAddress">Conjunto Residencial - Bloque/Torre - Apartamento/Casa</Label>
-                  <Input id="streetAddress" placeholder="Quintas de Don Simon - Bloque 2 - 204 (Ejemplo)" type="text" />
-                </LabelInputContainer>
-                <LabelInputContainer className="mb-4">
-                  <Label htmlFor="country">País</Label>
-                  <Input id="country" placeholder="Colombia (Ejemplo)" type="text" />
-                </LabelInputContainer>
-                <button 
-                  className="
-                    w-full inline-flex lg:h-16 md:h-15 sm:h-14 h-12  text-xs sm:text-sm md:text-xl animate-shimmer items-center justify-center z-40
-                    rounded-lg border border-[#5d9a8c] bg-[linear-gradient(110deg,#000103,45%,#75ba75,55%,#000103)] 
-                    bg-[length:200%_100%] px-3.5 font-normal md:font-medium text-slate-100 transition-colors focus:outline-none 
-                    focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50
-                  "
-                >
-                  Realizar pedido<WhatsAppIcon className="ml-2 h-3.5 sm:h-3.8 md:h-4 lg:h-5" />
-                </button>
-                <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-              </form>
+
+                  <LabelInputContainer className="mb-4">
+                    <Label htmlFor="streetAddress">Conjunto Residencial - Bloque - Casa</Label>
+                    <Input id="streetAddress" placeholder="Naturezza, Bloque 2, Apto. 212 (Ejemplo)" type="text" />
+                  </LabelInputContainer>
+
+                  <LabelInputContainer className="mb-4">
+                    <Label htmlFor="country">País</Label>
+                    <Input id="country" placeholder="Colombia (Ejemplo)" type="text" />
+                  </LabelInputContainer>
+
+                  <button 
+                    className="
+                      w-full inline-flex lg:h-16 md:h-15 sm:h-14 h-12  text-xs sm:text-sm md:text-xl animate-shimmer items-center justify-center z-40
+                      rounded-lg border border-[#5d9a8c] bg-[linear-gradient(110deg,#000103,45%,#75ba75,55%,#000103)] 
+                      bg-[length:200%_100%] px-3.5 font-normal md:font-medium text-slate-100 transition-colors focus:outline-none 
+                      focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50
+                    "
+                  >
+                    Realizar pedido
+                    <WhatsAppIcon className="ml-2 h-3.5 sm:h-3.8 md:h-4 lg:h-5" />
+                  </button>
+                  <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+                </form>
               </div>
+            )}
             </div>
         </section>
         <section className="w-full mt-20 inset-0 h-screen">
@@ -169,15 +206,6 @@ export default function CartPage() {
   )
 };
 
-
-const BottomGradient = () => {
-  return (
-    <>
-      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
-      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
-    </>
-  );
-};
 
 const LabelInputContainer = ({ children, className }) => {
   return (
